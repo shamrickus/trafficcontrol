9cb9d7eb22f22ddb7ec6d695444ab19195f5ca47
shamrickus <shamrickus@gmail.com>
2020-06-08 17:43:04 -0600

    Always return

--- lib/go-atscfg/remapdotconfig.go
+++ lib/go-atscfg/remapdotconfig.go
@@ -378,10 +378,8 @@ func MidHeaderRewriteConfigFileName(dsName string) string {
 
 // GetQStringIgnoreRemap returns the remap, whether cacheurl was added, and whether cachekey was added.
 func GetQStringIgnoreRemap(atsMajorVersion int) (string, bool, bool) {
-	if atsMajorVersion >= 6 {
-		return ` @plugin=cachekey.so @pparam=--separator= @pparam=--remove-all-params=true @pparam=--remove-path=true @pparam=--capture-prefix-uri=/^([^?]*)/$1/`, false, true
-	} else {
+	if atsMajorVersion < 7 {
 		log.Errorf("Unsupport version of ats found %v", atsMajorVersion)
-		return "", false, false
 	}
+	return ` @plugin=cachekey.so @pparam=--separator= @pparam=--remove-all-params=true @pparam=--remove-path=true @pparam=--capture-prefix-uri=/^([^?]*)/$1/`, false, true
 }
