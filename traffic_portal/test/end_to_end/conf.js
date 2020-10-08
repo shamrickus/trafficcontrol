9cdff99df67d62c87227a8475ccabd0b2fbe9415
shamrickus <shamrickus@gmail.com>
2020-10-08 16:48:05 -0600

    onPrepare dont wait

--- traffic_portal/test/end_to_end/conf.js
+++ traffic_portal/test/end_to_end/conf.js
@@ -25,3 +25,7 @@ exports.config = JSON.parse(
     path.join(process.cwd(), 'conf.json')
   ).toString()
 );
+
+exports.config.onPrepare = async function(){
+  await browser.waitForAngularEnabled(false);
+};
