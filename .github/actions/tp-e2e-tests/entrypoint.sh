23c3917811d81fd4ecb165a8605d730d6cc8e127
shamrickus <shamrickus@gmail.com>
2020-10-11 14:12:29 -0600

    Switch back

--- .github/actions/tp-e2e-tests/entrypoint.sh
+++ .github/actions/tp-e2e-tests/entrypoint.sh
@@ -90,7 +90,7 @@ mv /database.json ./database.conf
 
 cd "$SRCDIR/trafficcontrol/traffic_portal"
 gem update --system > /dev/null
-gem install sass compass rdoc rake > /dev/null
+gem install sass compass > /dev/null
 npm i --save-dev > /dev/null
 bower install --allow-root > /dev/null
 grunt dist
