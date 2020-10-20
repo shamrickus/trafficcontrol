d346dea2ebbe33619aea4efaf8590ee59cf41fb4
shamrickus <shamrickus@gmail.com>
2020-10-20 09:56:10 -0600

    Different password

--- .github/actions/tp-e2e-tests/entrypoint.sh
+++ .github/actions/tp-e2e-tests/entrypoint.sh
@@ -125,7 +125,7 @@ while ! curl -Lvsk "${fqdn}api/3.0/ping" 2>/dev/null >/dev/null; do
   sleep 2
 done
 
-psql -d postgresql://traffic_ops:twelve@postgres:5432/traffic_ops -c "SELECT count(*) FROM capability; SELECT * FROM users;"
+psql -d postgresql://traffic_ops:twelve@postgres:5432/traffic_ops -c "SELECT count(*) FROM capability; SELECT * FROM tm_user;"
 
 toget -k --to-url https://localhost:6443 --to-user admin --to-pass twelve logs
 toget -k --to-url https://localhost:6443 --to-user admin --to-pass twelve12 logs
