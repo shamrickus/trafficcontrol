a30dddf87163451f6569750639311aba619649c7
shamrickus <shamrickus@gmail.com>
2020-10-21 10:09:26 -0600

    Revert

--- .github/actions/tp-e2e-tests/entrypoint.sh
+++ .github/actions/tp-e2e-tests/entrypoint.sh
@@ -18,9 +18,6 @@
 
 set -e
 
-openssl -h
-exit;
-
 download_go() {
 	go_version="$(cat "${GITHUB_WORKSPACE}/GO_VERSION")"
 	wget -O go.tar.gz "https://dl.google.com/go/go${go_version}.linux-amd64.tar.gz"
