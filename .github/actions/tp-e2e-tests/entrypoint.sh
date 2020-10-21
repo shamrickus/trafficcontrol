d0af162b4a21a3b902956faeac644daada657768
shamrickus <shamrickus@gmail.com>
2020-10-21 08:53:03 -0600

    Use builtin

--- .github/actions/tp-e2e-tests/entrypoint.sh
+++ .github/actions/tp-e2e-tests/entrypoint.sh
@@ -98,17 +98,15 @@ mv /database.json ./database.conf
 
 ./traffic_ops_golang --cfg ./cdn.conf --dbcfg ./database.conf >out.log 2>err.log &
 
-BROWSER_HOST="hub"
-
 cd "$SRCDIR/trafficcontrol/traffic_portal"
 npm i --save-dev >/dev/null 2>&1
 bower install --allow-root >/dev/null 2>&1
 
 grunt dist >/dev/null 2>&1
 
-#webdriver-manager start >webdriver.log 2>&1 &
+webdriver-manager start >webdriver.log 2>&1 &
 
-fqdn="http://$BROWSER_HOST:4444/wd/hub/status"
+fqdn="http://localhost:4444/wd/hub/status"
 while ! curl -Lvsk "${fqdn}" >/dev/null 2>&1; do
   echo "waiting for selemnium server to start on '${fqdn}'"
   sleep 2
@@ -125,7 +123,7 @@ while ! curl -Lvsk "${fqdn}api/3.0/ping" >/dev/null 2>&1; do
   sleep 2
 done
 
-psql -d postgresql://traffic_ops:twelve@postgres:5432/traffic_ops -c "INSERT INTO tm_user (username, local_passwd, role, tenant_id) VALUES ('admin','SCRYPT:16384:8:1:vVw4X6mhoEMQXVGB/ENaXJEcF4Hdq34t5N8lapIjDQEAS4hChfMJMzwwmHfXByqUtjmMemapOPsDQXG+BAX/hA==:vORiLhCm1EtEQJULvPFteKbAX2DgxanPhHdrYN8VzhZBNF81NRxxpo7ig720KcrjH1XFO6BUTDAYTSBGU9KO3Q==', 1, 1)" >/dev/null 2>&1
+psql -d postgresql://traffic_ops:twelve@postgres:5432/traffic_ops -c "INSERT INTO tm_user (username, local_passwd, role, tenant_id) VALUES ('admin', 'SCRYPT:16384:8:1:vVw4X6mhoEMQXVGB/ENaXJEcF4Hdq34t5N8lapIjDQEAS4hChfMJMzwwmHfXByqUtjmMemapOPsDQXG+BAX/hA==:vORiLhCm1EtEQJULvPFteKbAX2DgxanPhHdrYN8VzhZBNF81NRxxpo7ig720KcrjH1XFO6BUTDAYTSBGU9KO3Q==', 1, 1)" >/dev/null 2>&1
 
 cd "test/end_to_end"
 mv /conf.json .
