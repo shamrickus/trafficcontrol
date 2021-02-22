d3039da3a8bc97fe3db4bac5a9e9792d0169fe5a
shamrickus <shamrickus@gmail.com>
2021-02-22 16:55:46 -0700

    no global

--- .github/actions/tp-integration-tests/entrypoint.sh
+++ .github/actions/tp-integration-tests/entrypoint.sh
@@ -202,10 +202,11 @@ done
 fqdn="https://localhost:6443"
 cd "test/integration"
 
+# Remove deps that we have installed globally (or are in a separate container) as they have precedence on the PATH
 jq 'del(.dependencies.protractor) | del(.dependencies.chromedriver) | del(.dependencies["selenium-webdriver"])' \
   package.json > package.json.tmp && mv package.json.tmp package.json
   
-sudo npm i -g --save-dev
+npm i --save-dev
 #remove
 echo "|START|"
 cp ${resources}/config.json .
