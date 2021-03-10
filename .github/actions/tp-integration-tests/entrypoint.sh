6703437a637c53be1607512ca5b00e5b196eba79
shamrickus <shamrickus@gmail.com>
2021-03-09 20:14:27 -0700

    Use sudo

--- .github/actions/tp-integration-tests/entrypoint.sh
+++ .github/actions/tp-integration-tests/entrypoint.sh
@@ -213,12 +213,14 @@ cd "test/integration"
 CONTAINER=$(docker ps | grep "selenium/node-chrome" | awk '{print $1}')
 CHROME_VER=$(docker exec "$CONTAINER" google-chrome --version | sed -E 's/.* ([0-9.]+).*/\1/')
 
-jq "del(.dependencies.chromedriver)" package.json > package.json.tmp && mv package.json.tmp package.json
+jq "del(.dependencies.chromedriver) | del(.dependencies.webdriver-manager) | del(.dependencies.selenium-webdriver)" package.json > package.json.tmp && mv package.json.tmp package.json
 npm i --save-dev
+
+npm i -g webdriver-manager
 
 PATH=$PATH:$(pwd)/node_modules/.bin/
 
-webdriver-manager update --gecko false --versions.chrome "LATEST_RELEASE_$CHROME_VER"
+sudo webdriver-manager update --gecko false --versions.chrome "LATEST_RELEASE_$CHROME_VER"
 
 #chromedriver_bin=$(./node_modules/.bin/chromedriver -v | awk '{print $2}')
 #
